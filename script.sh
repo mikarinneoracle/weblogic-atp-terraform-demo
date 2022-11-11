358e6482ece5cf4711baf465a8ea72ac17ffed16
Mika Rinne <mika.rinne@oracle.com>
2022-11-11 14:13:48 +0100

    create

--- script.sh
+++ script.sh
@@ -22,6 +22,7 @@ if [ -z "$OCID" ]; then
         sleep 10
     done
 fi
+clear
 echo "Terraform done. Doing maven build .."
 cd app
 mvn package -q
@@ -37,18 +38,21 @@ sed -i "s|SOURCE_URI|$preauth|g" cmd.json
 oci os object put --force -bn wls-artifacts --file app/target/app.war
 oci os object put --force -bn wls-artifacts --file wls.sh
 echo "Pushing artifacts done."
-echo "Waiting 3 mins WLS to start up .. get a coffee ;)"
+echo "Waiting WLS to start up .. get a coffee ;)"
+export ip=$(oci compute instance list-vnics --instance-id $OCID | jq -r '.data[]."public-ip"')
 spin='-\|/'
 export tries=0
+export res=''
 i=0
-while [ $tries -le 180 ]
+while [ $tries -le 600 ] && [[ $res == '' ]]
 do
   i=$(( (i+1) %4 ))
   printf "\r${spin:$i:1}"
+  export res=$(curl http://$ip:7001 | grep TITLE)
   sleep 1
   tries=$(( $tries + 1 ))
 done
-echo "Starting app deployment using VM instance agent ..."
+echo "WebLogic running ... starting app deployment using VM instance-agent ..."
 export cmd=$(oci instance-agent command create --from-json file://cmd.json | jq '.data.id' | tr -d '"')
 export tries=0
 export status=''
