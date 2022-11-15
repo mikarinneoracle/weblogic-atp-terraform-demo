1d1f45088846bb58a42ea28d050ca59af0395f42
Mika Rinne <mika.rinne@oracle.com>
2022-11-15 17:27:43 +0100

    db stuff added

--- terraform/atp.tf
+++ terraform/atp.tf
@@ -1,9 +1,5 @@
 resource "oci_database_autonomous_database" "weblogic_atp" {
-
-  # Let's use the public access instead of private 
-  # in order to be able to run the Liquibase stuff with TF local-exec
-  
-  subnet_id                = oci_core_subnet.Public_Subnet_weblogic.id
+  subnet_id                = oci_core_subnet.Private_Subnet_weblogic.id
   private_endpoint_label   = "weblogic"
   admin_password           = var.weblogic_atp_admin_password
   is_free_tier             = var.use_always_free
@@ -15,7 +11,6 @@ resource "oci_database_autonomous_database" "weblogic_atp" {
   is_dedicated             = false
   cpu_core_count           = 1
   data_storage_size_in_tbs = 1
-  license_model            = "LICENSE_INCLUDED"
   is_preview_version_with_service_terms_accepted = false
   freeform_tags = {
     Managed = var.tags
--- terraform/vcn.tf
+++ terraform/vcn.tf
@@ -21,6 +21,29 @@ resource "oci_core_internet_gateway" "weblogic_IG" {
   vcn_id = oci_core_vcn.weblogic_vcn.id
 }
 
+resource "oci_core_route_table" "weblogic_private_rt" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.weblogic_vcn.id
+  display_name   = "Route Table for Private Subnet-wls"
+  freeform_tags = {
+    Managed = var.tags
+  }
+
+  route_rules {
+    network_entity_id = oci_core_nat_gateway.weblogic_nat_gw.id
+    description       = "Internet via NAT Gateway"
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+
+  route_rules {
+    network_entity_id = oci_core_service_gateway.weblogic_service_gateway.id
+    description       = "OCI Services via Service Gateway"
+    destination       = "all-ams-services-in-oracle-services-network"
+    destination_type  = "SERVICE_CIDR_BLOCK"
+  }
+}
+
 resource "oci_core_default_route_table" "weblogic_public_rt" {
   compartment_id = var.compartment_id
   display_name = "Default Route Table for wls"
@@ -34,13 +57,6 @@ resource "oci_core_default_route_table" "weblogic_public_rt" {
     destination       = "0.0.0.0/0"
     destination_type  = "CIDR_BLOCK"
     network_entity_id = oci_core_internet_gateway.weblogic_IG.id
-  }
-
-  route_rules {
-    network_entity_id = oci_core_service_gateway.weblogic_service_gateway.id
-    description       = "OCI Services via Service Gateway"
-    destination       = "all-ams-services-in-oracle-services-network"
-    destination_type  = "SERVICE_CIDR_BLOCK"
   }
 }
 
@@ -86,6 +102,42 @@ resource "oci_core_security_list" "weblogic_security_list_public" {
     source_type = "CIDR_BLOCK"
   }
 
+  freeform_tags = {
+    Managed = var.tags
+  }
+}
+
+resource "oci_core_security_list" "weblogic_security_list_private" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.weblogic_vcn.id
+  display_name   = "Security list for WebLogic private subnet"
+
+  egress_security_rules {
+    destination      = "0.0.0.0/0"
+    protocol         = "all"
+    destination_type = "CIDR_BLOCK"
+  }
+
+  ingress_security_rules {
+    protocol = "1"
+    source   = "0.0.0.0/0"
+    icmp_options {
+      type = "3"
+      code = "4"
+    }
+    source_type = "CIDR_BLOCK"
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+    source_type = "CIDR_BLOCK"
+  }
+
   ingress_security_rules {
     protocol = "6"
     source   = "0.0.0.0/0"
@@ -101,9 +153,18 @@ resource "oci_core_security_list" "weblogic_security_list_public" {
   }
 }
 
+resource "oci_core_nat_gateway" "weblogic_nat_gw" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.weblogic_vcn.id
+  block_traffic  = false
+  display_name   = "weblogic NGW"
+  freeform_tags = {
+    Managed = var.tags
+  }
+}
+
 resource "oci_core_subnet" "Public_Subnet_weblogic" {
   vcn_id              = oci_core_vcn.weblogic_vcn.id
-  dns_label           = "weblogic"
   availability_domain = var.availability_domain
   cidr_block          = "10.0.0.0/24"
   compartment_id      = var.compartment_id
@@ -117,6 +178,24 @@ resource "oci_core_subnet" "Public_Subnet_weblogic" {
   prohibit_internet_ingress  = "false"
   prohibit_public_ip_on_vnic = "false"
   route_table_id             = oci_core_default_route_table.weblogic_public_rt.id
+}
+
+resource "oci_core_subnet" "Private_Subnet_weblogic" {
+  vcn_id              = oci_core_vcn.weblogic_vcn.id
+  dns_label           = "weblogic"
+  availability_domain = var.availability_domain
+  cidr_block          = "10.0.1.0/24"
+  compartment_id      = var.compartment_id
+  display_name        = "Private Subnet-wls"
+  freeform_tags       = {
+    Managed = var.tags
+  }
+  security_list_ids = [
+     oci_core_security_list.weblogic_security_list_private.id
+  ]
+  prohibit_internet_ingress  = "true"
+  prohibit_public_ip_on_vnic = "true"
+  route_table_id             = oci_core_route_table.weblogic_private_rt.id
 }
 
 resource "oci_core_service_gateway" "weblogic_service_gateway" {
