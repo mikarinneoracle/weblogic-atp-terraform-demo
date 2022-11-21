66423ed7818833e0e7d98976efafd675565b0f8b
Mika Rinne <mika.rinne@oracle.com>
2022-11-21 12:29:54 +0100

    tf destroy script added

--- script.sh
+++ script.sh
@@ -62,9 +62,3 @@ do
     tries=1001
   fi
 done
-export par=$(oci os preauth-request list -bn wls-artifacts | jq '.data[].id' | tr -d '"')
-oci os preauth-request delete -bn wls-artifacts --par-id $par --force
-oci os object bulk-delete -bn wls-artifacts --force
-oci os object bulk-delete -bn wls-create-domain --force
-rm -f out.txt
-rm -f tf.out
