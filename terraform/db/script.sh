1ca5a98144e25341356abea1acb1fc8b56101863
Mika Rinne <mika.rinne@oracle.com>
2023-08-02 10:24:44 +0300

    Update script.sh
    PAR updated

--- terraform/db/script.sh
+++ terraform/db/script.sh
@@ -6,11 +6,11 @@ mkdir -p ./network/admin
 mv wallet.zip ./network/admin/
 
 echo "------------- quick fix to run with older version of SQLcli (scripts fail with the latest Nov 27th 2022) ----------"
-wget https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/BydeV7ct283Y4F2S9PECPEyeNI-U3xoaghdLQ0EuzNUEDIMoieyqu5uA7xJ-syyq/n/frsxwtjslf35/b/oracledb/o/jdk-11.0.16_linux-x64_bin.tar.gz -q
+wget https://frsxwtjslf35.objectstorage.eu-amsterdam-1.oci.customer-oci.com/p/HQRnyCTvmQXuS9rcttx5HsrSK8Vn-PINn-Rn6WjPMArHjjQmQVuaylSrA-oR2hCM/n/frsxwtjslf35/b/oracledb/o/jdk-11.0.16_linux-x64_bin.tar.gz -q
 tar -xzf jdk-11.0.16_linux-x64_bin.tar.gz
 export PATH=./jdk-11.0.16/bin:$PATH
 export JAVA_HOME=./jdk-11.0.16
-wget https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/rBz7NIZQsEXsXN6yqnLn8m9fLmTHZUY7Z5uhPBBUzsoiW0ceUoY1jyU5y7DjEWJx/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
+wget https://frsxwtjslf35.objectstorage.eu-amsterdam-1.oci.customer-oci.com/p/iwLFugXvt9Ww0CkJxRrYQBOT6YXh-Sofs5mu2-qE-Li_locJC19JBB_iQgV7plmn/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
 unzip -q V1022102-01.zip
 ./sqlcl/bin/sql -v
 echo "-------------------------------------------------------------------------------------------------------------------"
