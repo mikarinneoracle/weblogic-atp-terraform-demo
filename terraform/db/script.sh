fe5af2f1276d2ad92b9c1bc605749df3fa579050
Mika Rinne <mika.rinne@oracle.com>
2023-11-09 13:41:24 +0200

    Update script.sh
    fixes

--- terraform/db/script.sh
+++ terraform/db/script.sh
@@ -10,7 +10,7 @@ wget https://frsxwtjslf35.objectstorage.eu-amsterdam-1.oci.customer-oci.com/p/HQ
 tar -xzf jdk-11.0.16_linux-x64_bin.tar.gz
 export PATH=./jdk-11.0.16/bin:$PATH
 export JAVA_HOME=./jdk-11.0.16
-wget https://frsxwtjslf35.objectstorage.eu-amsterdam-1.oci.customer-oci.com/p/iwLFugXvt9Ww0CkJxRrYQBOT6YXh-Sofs5mu2-qE-Li_locJC19JBB_iQgV7plmn/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
+wget https://frsxwtjslf35.objectstorage.eu-amsterdam-1.oci.customer-oci.com/p/HQRnyCTvmQXuS9rcttx5HsrSK8Vn-PINn-Rn6WjPMArHjjQmQVuaylSrA-oR2hCM/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
 unzip -q V1022102-01.zip
 ./sqlcl/bin/sql -v
 echo "-------------------------------------------------------------------------------------------------------------------"
